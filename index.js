852c9d6a2309790d8c688c48cb85c57bcaa715ad
Donaldo Arriaza <donaldoarriaza@gmail.com>
2022-02-27 21:50:45 -0600

    terminado

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 var Mousetrap = require('mousetrap')
 var botonUno = document.getElementById('botonUno')
 var botonDos = document.getElementById('botonDos')
+var botonTres = document.getElementById('botonTres')
+var botonCuatro = document.getElementById('botonCuatro')
+var botonCinco = document.getElementById('botonCinco')
+var botonSeis = document.getElementById('botonSeis')
+var botonSiete = document.getElementById('botonSiete')
+var botonOcho = document.getElementById('botonOcho')
+var botonNueve = document.getElementById('botonNueve')
+var botonCero = document.getElementById('botonCero')
 var botonSuma = document.getElementById('botonSuma')
+var botonResta = document.getElementById('botonResta')
+var botonMultiplicacion = document.getElementById('botonMultiplicacion')
+var botonDivision = document.getElementById('botonDivision')
+var botonBorrar = document.getElementById('botonBorrar')
+var botonReset = document.getElementById('botonReset')
 var actual = document.getElementById('actual')
 var resultado = document.getElementById('resultado')
 
 
 botonUno.addEventListener('click', clickUno)
 botonDos.addEventListener('click', clickDos)
+botonTres.addEventListener('click', clickTres)
+botonCuatro.addEventListener('click', clickCuatro)
+botonCinco.addEventListener('click', clickCinco)
+botonSeis.addEventListener('click', clickSeis)
+botonSiete.addEventListener('click', clickSiete)
+botonOcho.addEventListener('click', clickOcho)
+botonNueve.addEventListener('click', clickNueve)
+botonCero.addEventListener('click', clickCero)
+
 botonSuma.addEventListener('click', clickSuma)
+botonMultiplicacion.addEventListener('click', clickMutiplicacion)
+botonResta.addEventListener('click', clickResta)
+botonDivision.addEventListener('click', clickDivision)
+botonBorrar.addEventListener('click', clickBorrar)
+botonReset.addEventListener('click', clickReset)
 
 Mousetrap.bind('1', clickUno)
+Mousetrap.bind('2', clickDos)
+Mousetrap.bind('3', clickTres)
+Mousetrap.bind('4', clickCuatro)
+Mousetrap.bind('5', clickCinco)
+Mousetrap.bind('6', clickSeis)
+Mousetrap.bind('7', clickSiete)
+Mousetrap.bind('8', clickOcho)
+Mousetrap.bind('9', clickNueve)
+Mousetrap.bind('0', clickCero)
+Mousetrap.bind('+', clickSuma)
+Mousetrap.bind('-', clickResta)
+Mousetrap.bind('*', clickMutiplicacion)
+Mousetrap.bind('/', clickDivision)
+Mousetrap.bind('del', clickBorrar)
+
 
 function clickUno() {
     actual.innerHTML += '1'
@@ -20,9 +62,77 @@ function clickDos() {
     actual.innerHTML += '2'
 }
 
+function clickTres() {
+    actual.innerHTML += '3'
+}
+
+function clickCuatro() {
+    actual.innerHTML += '4'
+}
+
+function clickCinco() {
+    actual.innerHTML += '5'
+}
+
+function clickSeis() {
+    actual.innerHTML += '6'
+}
+
+function clickSiete() {
+    actual.innerHTML += '7'
+}
+
+function clickOcho() {
+    actual.innerHTML += '8'
+}
+
+function clickNueve() {
+    actual.innerHTML += '9'
+}
+
+function clickCero() {
+    actual.innerHTML += '0'
+}
+
 function clickSuma() {
     let valorActual = parseInt(actual.innerHTML)
     let valorResultado = parseInt(resultado.innerHTML)
     resultado.innerHTML = valorActual + valorResultado
     actual.innerHTML = '0'
+}
+
+function clickResta() {
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorResultado - valorActual
+    actual.innerHTML = '0'
+}
+
+function clickMutiplicacion() {
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorActual * valorResultado
+    actual.innerHTML = '0'
+}
+
+function clickDivision() {
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorResultado / valorActual
+    actual.innerHTML = '0'
+
+
+}
+
+function clickBorrar() {
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    actual.innerHTML = '0'
+}
+
+function clickReset() {
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    actual.innerHTML = '0'
+    resultado.innerHTML = '0'
 }
